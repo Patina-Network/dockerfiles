c57b614a371cea197284008496c7878dfc6b8870
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-23 01:13:12 -0400

    Setup build images workflow

--- .github/scripts/src/build-images/index.ts
+++ .github/scripts/src/build-images/index.ts
@@ -2,17 +2,25 @@ import {
   DockerClient,
   EnvClient,
   EnvClientStrategy,
+  Utils,
 } from "@tahminator/pipeline";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-const { tag } = await yargs(hideBin(process.argv))
-  .option("tag", {
+const { ver, shouldUpload } = await yargs(hideBin(process.argv))
+  .option("ver", {
     type: "string",
     demandOption: true,
   })
+  .option("shouldUpload", {
+    type: "boolean",
+    demandOption: true,
+    default: true,
+  })
   .strict()
   .parse();
+
+const targets = ["pg-az"] as const;
 
 export async function main() {
   const envClient = EnvClient.create(EnvClientStrategy.SOPS);
@@ -24,12 +32,17 @@ export async function main() {
     dockerHubPat,
   );
 
-  await dockerClient.buildImage({
-    dockerFileLocation: "src/Dockerfile",
-    shouldUpload: true,
-    dockerRepository: "pg-az",
-    tags: ["latest", tag],
-  });
+  for (const target of targets) {
+    const dir = `src/${target}` as const;
+    const dockerFileLocation = `${dir}/Dockerfile` as const;
+    console.log(Utils.Colors.cyan(`Building ${dockerFileLocation}...`));
+    await dockerClient.buildImage({
+      dockerFileLocation,
+      shouldUpload,
+      dockerRepository: target,
+      tags: ["latest", ver],
+    });
+  }
 }
 
 function parseCiEnv(ciEnv: Record<string, string>) {
@@ -51,3 +64,12 @@ function parseCiEnv(ciEnv: Record<string, string>) {
 
   return { dockerHubPat, dockerHubUsername };
 }
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
